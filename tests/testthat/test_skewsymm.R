092aa072e5555eab9f8eea602246496a1a5ebac8
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-11-28 10:01:02 +0100

    Now using Matrix for skew-symm decomposition

--- tests/testthat/test_skewsymm.R
+++ tests/testthat/test_skewsymm.R
@@ -3,26 +3,24 @@ context('Skew symmetric decomposition')
 test_that('Test symm',
           {
             A <- matrix(c(1,2,2,1), 2, 2)
-            B <- skewsymm(A)
 
             symm_expected <- A
             skew_expected <- matrix(0, 2, 2)
 
-            expect_equal(B$symm, symm_expected)
-            expect_equal(B$skew, skew_expected)
+            expect_equal(Matrix::symmpart(A), symm_expected)
+            expect_equal(Matrix::skewpart(A), skew_expected)
           }
 )
 
 test_that('Test skew',
           {
             A <- matrix(c(0,2,-2,0), 2, 2)
-            B <- skewsymm(A)
 
             symm_expected <- matrix(0, 2, 2)
             skew_expected <- A
 
-            expect_equal(B$symm, symm_expected)
-            expect_equal(B$skew, skew_expected)
+            expect_equal(Matrix::symmpart(A), symm_expected)
+            expect_equal(Matrix::skewpart(A), skew_expected)
           }
 )
 
@@ -32,10 +30,9 @@ test_that('Test combined',
             skew_expected <- matrix(c(0, 1, -1, 0), 2, 2)
 
             A <- symm_expected + skew_expected
-            B <- skewsymm(A)
 
-            expect_equal(B$symm, symm_expected)
-            expect_equal(B$skew, skew_expected)
+            expect_equal(Matrix::symmpart(A), symm_expected)
+            expect_equal(Matrix::skewpart(A), skew_expected)
           }
 )
 
@@ -43,6 +40,7 @@ test_that('Error',
           {
             A_non_square <- matrix(0, 2, 3)
 
-            expect_error(skewsymm(A_non_square))
+            expect_error(Matrix::symmpart(A_non_square))
+            expect_error(Matrix::skewpart(A_non_square))
           }
 )
